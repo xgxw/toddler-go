bc7a2e78d4ae58f0c89308f41157cb88f62bdb6f
wzs <zhensheng.five@gmail.com>
2020-04-13 22:03:14 +0800

    bootstrap 添加描述介绍

--- internal/cmd/bootstrap.go
+++ internal/cmd/bootstrap.go
@@ -10,6 +10,24 @@ import (
 	"github.com/xgxw/toddler-go/internal/services"
 )
 
+/*
+	Bootstrap 提供统一的资源创建. 这么做有如下优点
+	1. 依赖注入/控制反转: 将资源的创建操作统一放到 Bootstrap, 由 Bootstrap 协调.
+	2. 门面模式: 提供一个统一的类去访问多个子系统的多个不同的资源.
+
+	这么做的好处有
+	1. 后续资源创建方法更改后, 只需要更改Bootstrap中的创建方法, 不需要改动其他地方.
+	2. 统一的创建减少代码重复度, 让代码更简洁直观.
+
+	刚开始时, 是以成员字段的方式提供资源, 现在改为 GetLogger() 的方式提供资源.
+	新的方式更灵活, 更能适应多个Command的情况.
+	如 server/grpc 需要 logger/db/cache/demoSvc
+	task1 只需要 db
+	task2 只需要 demoSvc
+
+	使用这种方式可以更灵活的初始化, 避免task1运行时添加 cache/demoSvc 的配置
+*/
+
 type Bootstrap struct {
 	opts    *Options
 	logger  *flog.Logger
