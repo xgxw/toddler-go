3cd7dc3037dfa39a39320bc937ffb431551e5cde
wzs <zhensheng.five@gmail.com>
2020-05-11 20:38:09 +0800

    添加命名部分修改

--- demo.go
+++ demo.go
@@ -19,15 +19,21 @@ type (
 	}
 )
 
-// DemoStruct 结构体定义示例, gorm 会自动设置 CreatedAt/UpdatedAt/DeletedAt
-type DemoStruct struct {
-	ID   int    `gorm:"column:id" json:"id"`
-	Name string `gorm:"column:name" json:"name"`
+type (
+	// DemoStructDTO 是 DemoStruct 对外展示的结构体,
+	// DemoStruct 即 DemoStructDO, 是数据库存储的结构体.
+	// 具体参考 link:/docs/namings[命名规范]
+	DemoStructDTO DemoStruct
+	// DemoStruct 结构体定义示例, gorm 会自动设置 CreatedAt/UpdatedAt/DeletedAt
+	DemoStruct struct {
+		ID   int    `gorm:"column:id" json:"id"`
+		Name string `gorm:"column:name" json:"name"`
 
-	CreatedAt time.Time  `gorm:"column:created_at" json:"-"`
-	UpdatedAt time.Time  `gorm:"column:updated_at" json:"-"`
-	DeletedAt *time.Time `gorm:"column:deleted_at" json:"-"`
-}
+		CreatedAt time.Time  `gorm:"column:created_at" json:"-"`
+		UpdatedAt time.Time  `gorm:"column:updated_at" json:"-"`
+		DeletedAt *time.Time `gorm:"column:deleted_at" json:"-"`
+	}
+)
 
 // TableName gorm 规范, 需要添加 TableName 以便gorm获取表名.
 // 规范为 "业务名+s", 连字符模式. 如 user=> users.
